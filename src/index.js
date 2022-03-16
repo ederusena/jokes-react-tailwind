23b300b4e92610bef37130fb95f7c1f27d59cf4f
Eder Sena <eder.sena@live.com>
2022-03-16 14:17:40 -0300

    chore: Jokes

--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ThemeProvider } from './components/ThemeContext';
+import Background from './components/Background';
+import Toggle from './components/ThemeToggle';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <Background>
+        <div className="absolute right-0 top-0 mr-4 mt-4 md:mr-6 md:mt-6">
+          <Toggle />
+        </div>
+
+        <App />
+      </Background>
+    </ThemeProvider>
+    ,
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
